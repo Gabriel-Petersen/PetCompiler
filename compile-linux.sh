7376fa1550da3e244209c81cafbf163a4103b0e3
Gabriel-Petersen <gabrielf.petersen@gmail.com>
2026-08-24 14:27:22 -0300

    Linux build fixed

--- compile-linux.sh
+++ compile-linux.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-MODE=$1
+set -e
 
-if [ "$MODE" == "--force"]; then
+MODE="${1:-}"
+
+if [ "$MODE" = "--force" ]; then
     if [ -d "build" ]; then
         echo "Limpando a pasta build antiga..."
         rm -rf build
     fi
 fi
 
-cmake -S . -B build -G Ninja -DCMAKE_CXX_COMPILER=/usr/bin/g++-14
+cmake -S . -B build -G Ninja -DCMAKE_CXX_COMPILER=/usr/bin/g++-16
 
 cmake --build build
